e70673966fd14b06e56287b1e14fcce6b569f177
Sebastien Prud'homme <sebastien.prudhomme-gateau@etu.umontpellier.fr>
2024-01-06 21:41:02 +0100

    rendu

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -25,30 +25,37 @@ def execute_java_program(data_file, memory):
     subprocess.run(command)
     return
 
-def run(ram, data):
-    print(f"\nexecution sur RDFEngine avec {ram}G de Heap Space et {data} de données\n")
-    execute_java_program(f"./data/data/{data}", ram)
+def run():
+    for ram in memories:
+        for data in dataset:
+            print(f"\nexecution sur RDFEngine avec {ram}G de Heap Space et {data} de données\n")
+            execute_java_program(f"./data/data/{data}", ram)
 
+# def run(ram, data):
+#     print(f"\nexecution sur RDFEngine avec {ram}G de Heap Space et {data} de données\n")
+#     execute_java_program(f"./data/data/{data}", ram)
 
 if __name__ == "__main__":
     print("Lancement du benchmark\n")
 
-    print("veuillez indiquer la quantité de ram à allouer\n")
-    ram = input()
+    # print("veuillez indiquer la quantité de ram à allouer\n")
+    # ram = input()
+    #
+    # print("veuillez indiquer le fichier de données à utiliser (1 ou 2)\n")
+    # d = input("1 : 500K.nt\n2 : 2M.nt\n")
+    # try :
+    #     d = int(d)
+    # except ValueError:
+    #     print("veuillez entrer un nombre valide")
+    #     exit()
+    # if d < 0:
+    #     print("veuillez entrer un nombre valide")
+    #     exit()
+    #
+    # data = dataset[d-1]
+    #
+    # run(ram, data)
 
-    print("veuillez indiquer le fichier de données à utiliser (1 ou 2)\n")
-    d = input("1 : 500K.nt\n2 : 2M.nt\n")
-    try :
-        d = int(d)
-    except ValueError:
-        print("veuillez entrer un nombre valide")
-        exit()
-    if d < 0:
-        print("veuillez entrer un nombre valide")
-        exit()
-
-    data = dataset[d-1]
-
-    run(ram, data)
+    run()
 
 
--- scripts/multiplot.py
+++ scripts/multiplot.py
@@ -51,8 +51,8 @@ pos = np.arange(len(categories))
 width = 0.35  # epaisseur de chaque bâton
 
 # données
-qenginedata = g1.groupby(moteur_H).get_group('qengine')[[queryReadTime_H,workloadTime_H,totalTime_H]]
-jenadata = g1.groupby(moteur_H).get_group('jena')[[queryReadTime_H,workloadTime_H,totalTime_H]]
+qenginedata = g4.groupby(moteur_H).get_group('qengine')[[queryReadTime_H,workloadTime_H,totalTime_H]]
+jenadata = g4.groupby(moteur_H).get_group('jena')[[queryReadTime_H,workloadTime_H,totalTime_H]]
 
 qenginedata = qenginedata.transpose().squeeze().astype('int32')
 jenadata = jenadata.transpose().squeeze().astype('int32')
@@ -64,6 +64,6 @@ print(qenginedata)
 plt.bar(pos - width/2, qenginedata, width, color='IndianRed')
 plt.bar(pos + width/2, jenadata, width, color='SkyBlue')
 plt.xticks(pos, categories)
-plt.title('comparaison des temps d\'executions des moteurs pour 500K données et 2G de mémoire',fontsize=15)
+plt.title('comparaison des temps d\'executions des moteurs pour 2M données et 2G de mémoire',fontsize=15)
 plt.legend(moteur,loc=1)
 plt.show()
